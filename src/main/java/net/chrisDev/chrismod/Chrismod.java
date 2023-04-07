2479f83b06ab02af428acad4c161b802eff8cce7
chris <cs.communication.dev@gmail.com>
2023-04-07 14:30:44 +0200

    add: first item nammed amethyst

--- src/main/java/net/chrisDev/chrismod/Chrismod.java
+++ src/main/java/net/chrisDev/chrismod/Chrismod.java
@@ -1,15 +1,24 @@
 package net.chrisDev.chrismod;
 
+import net.chrisDev.chrismod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(Chrismod.MOD_ID)
 public class Chrismod
 {
     public static  final String MOD_ID="chrismod";
+
+    private static final Logger LOGGER = LogManager.getLogger();
     public Chrismod()
     {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();;
 
+        ModItems.ITEMS.register(eventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
---
+++ src/main/java/net/chrisDev/chrismod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.chrisDev.chrismod.item;
+
+import net.chrisDev.chrismod.Chrismod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Chrismod.MOD_ID);
+
+    public static final RegistryObject<Item> AMETHYST = register("amethyst",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)) );
+
+    private static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item){
+        return ITEMS.register(name,item);
+    }
+}
