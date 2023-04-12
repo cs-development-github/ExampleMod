19a30ddfa9eef9affd1c5fe3932df9600ce9caab
chris <cs.communication.dev@gmail.com>
2023-04-12 14:29:02 +0200

    add: citrine tools

--- src/main/java/net/chrisDev/chrismod/item/ModItems.java
+++ src/main/java/net/chrisDev/chrismod/item/ModItems.java
@@ -3,10 +3,7 @@ package net.chrisDev.chrismod.item;
 import net.chrisDev.chrismod.Chrismod;
 import net.chrisDev.chrismod.item.custom.CoalCokeItem;
 import net.chrisDev.chrismod.item.custom.DowsingRodItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -34,6 +31,21 @@ public class ModItems {
 
     public static final RegistryObject<Item> COAL_COKE = ITEMS.register("coal_coke",
             () -> new CoalCokeItem(new Item.Properties().tab(ModItemGroup.ChrisModTab)));
+
+    public static final RegistryObject<Item> CITRINE_SWORD = ITEMS.register("citrine_sword",
+            () -> new SwordItem(ModTiers.CITRINE , 2 , 3f , new Item.Properties().tab(ModItemGroup.ChrisModTab)));
+
+    public static final RegistryObject<Item> CITRINE_PICKAXE = ITEMS.register("citrine_pickaxe",
+            () -> new PickaxeItem(ModTiers.CITRINE , 2 , 3f , new Item.Properties().tab(ModItemGroup.ChrisModTab)));
+
+    public static final RegistryObject<Item> CITRINE_SHOVEL = ITEMS.register("citrine_shovel",
+            () -> new ShovelItem(ModTiers.CITRINE , 2 , 3f , new Item.Properties().tab(ModItemGroup.ChrisModTab)));
+
+    public static final RegistryObject<Item> CITRINE_AXE = ITEMS.register("citrine_axe",
+            () -> new AxeItem(ModTiers.CITRINE , 2 , 3f , new Item.Properties().tab(ModItemGroup.ChrisModTab)));
+
+    public static final RegistryObject<Item> CITRINE_HOE = ITEMS.register("citrine_hoe",
+            () -> new HoeItem(ModTiers.CITRINE , 2 , 3f , new Item.Properties().tab(ModItemGroup.ChrisModTab)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
